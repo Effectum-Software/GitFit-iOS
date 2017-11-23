ea758b7d7bc2a920d0558efe0243a80d12bff8eb
alex <vihlayew.alex@gmail.com>
2017-11-23 21:33:20 +0200

    Initial commit

---
+++ GitFit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitFit
+//
+//  Created by Alex Vihlayew on 23/11/2017.
+//  Copyright © 2017 Alex Vihlayew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
